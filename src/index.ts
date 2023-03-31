8240666fa6a729d1d47617d7185de3addd764211
Brett R. Gebbie <brg078@gmail.com>
2023-03-31 13:52:28 -0500

    orientationdone

--- src/index.ts
+++ src/index.ts
@@ -103,3 +103,37 @@ let textBox: UIWidget = {
     resize: () => {}
 }
 
+//Literal (Exact, specific)
+type Quantity = 50 | 100;
+let quantity: Quantity = 100;
+
+type Metric = 'cm' | 'inch';
+
+function greet(name: string | null | undefined) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log('Hola!');
+}
+
+greet(null);
+
+type Customer = {
+    birthday: Date
+};
+
+function getCustomer(id: number): Customer | null | undefined {
+    return id === 0 ? null : {birthday: new Date()}
+}
+
+let customer = getCustomer(0);
+//if (customer !== null && customer !==undefined)
+//Optional property access operator
+    console.log(customer?.birthday?.getFullYear());
+
+//optional element access operator
+//customers?.[0]
+
+//Optional call
+let log: any = null;
+log?.('a');
